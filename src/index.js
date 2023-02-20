63f5c69bc15365cf8f2e75eacbc34153563b8ba9
khalil ben romdhane <kh1925@outlook.com>
2023-02-20 23:01:54 +0100

    deleted files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*
+Objective
+In this checkpoint, we are going to create our first class-based component and we are going also to create our first state.
+
+ 
+
+Instructions
+Create a project using create-react-app.
+Transform the App.js into a class-based component.
+Implement a state for this class containing a Person ={ fullName,bio, imgSrc, profession} and a boolean shows.
+Add a button that toggles the show state. When the show state is true, the person's profile will appear.
+Create a field that shows the time interval since the last component was mounted using the component lifecycle.
+Hint: use the setInterval method.
+*/
